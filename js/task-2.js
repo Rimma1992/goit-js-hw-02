f98b88ceee8f0ccff035b74ef9aaad1c23a06449
Rimma1992 <149500783+Rimma1992@users.noreply.github.com>
2024-02-13 13:23:56 +0100

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,13 @@
 
-
+function formatMessage(message, maxLength) {
+  let result;
+if (message.length <= maxLength) {
+  result = message;
+} else {
+  result = message.slice(0, maxLength) + "...";
+}
+  return result;
+}
 
 
 
